982875b53e75c8d8925be3f16c2af3794fe70425
simon7070 <75843200+simon7070@users.noreply.github.com>
2021-05-21 15:41:52 +0800

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,4 +1,31 @@
+import java.awt.Color;
+import java.awt.Font;
 
-public class Transaction {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+
+public class Transaction extends JFrame{
+	ImageIcon image1 = new ImageIcon("sra.png");
+	
+	
+	Transaction() {
+    
+    
+	  JLabel lbl = new JLabel("TRANSACTION DETAILS");
+	   
+	    
+	    this.add(lbl);
+	    
+	   
+	  
+	    setBounds(300, 30, 300, 218); // THE POP UP FRAME
+	  	this.setSize(790,500);  
+	    this.setVisible(true); 
+	    this.setIconImage(image1.getImage());
+	    this.setTitle("Transaction Details");
+	    
+	 
+	}
 }
